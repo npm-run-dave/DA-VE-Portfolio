a9cf17edd1ba5fd8a1c8ed1af2d1a7398a4d32d1
Dave23x1 <ryandavedonos@gmail.com>
2025-03-05 13:38:59 +0800

    Chore

--- app/block/Techtacks.jsx
+++ app/block/Techtacks.jsx
@@ -6,44 +6,36 @@ export default function Carousel() {
         <div className="slide">
           <Image src="/vue.png" height="40" width="40" alt="" />
         </div>
-        <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
-        </div>
+
         <div className="slide">
           <Image src="/nextjs.png" height="40" width="40" alt="" />
         </div>
         <div className="slide">
           <Image src="/express.png" height="40" width="40" alt="" />
         </div>
+
+        <div className="slide">
+          <Image src="/mongodb-icon.png" height="40" width="40" alt="" />
+        </div>
+        <div className="slide">
+          <Image src="/tailwind.png" height="40" width="40" alt="" />
+        </div>
         <div className="slide">
           <Image src="/vue.png" height="40" width="40" alt="" />
         </div>
+
         <div className="slide">
           <Image src="/nextjs.png" height="40" width="40" alt="" />
         </div>
         <div className="slide">
           <Image src="/express.png" height="40" width="40" alt="" />
         </div>
+
         <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
+          <Image src="/mongodb-icon.png" height="40" width="40" alt="" />
         </div>
         <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
-        </div>
-        <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
-        </div>
-        <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
-        </div>
-        <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
-        </div>
-        <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
-        </div>
-        <div className="slide">
-          <Image src="/vue.png" height="40" width="40" alt="" />
+          <Image src="/tailwind.png" height="40" width="40" alt="" />
         </div>
       </div>
     </div>
